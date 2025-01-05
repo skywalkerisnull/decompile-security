877d4c33383e77c82dbaa502c211876c739eb357
Ashley <7260106+skywalkerisnull@users.noreply.github.com>
2025-01-05 11:33:28 +1000

    Trivial application initial commit

---
+++ decompile-security/Config.cs
@@ -0,0 +1,50 @@
+﻿using System;
+public class Config
+{
+    public DatabaseConfig Database { get; set; }
+    public ApiConfig Api { get; set; }
+    public ServiceAccountConfig ServiceAccount { get; set; }
+
+    public Config()
+    {
+        Database = new DatabaseConfig
+        {
+            Username = "root",
+            Password = "super_secret_password",
+            Host = "postgres.example.com",
+            Port = 5432
+        };
+
+        Api = new ApiConfig
+        {
+            Key = "api_key_1234567890",
+            Endpoint = "https://api.example.com"
+        };
+
+        ServiceAccount = new ServiceAccountConfig
+        {
+            ClientId = "client_id",
+            ClientSecret = "client_secret"
+        };
+    }
+
+    public struct DatabaseConfig
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Host { get; set; }
+        public int Port { get; set; }
+    }
+
+    public struct ApiConfig
+    {
+        public string Key { get; set; }
+        public string Endpoint { get; set; }
+    }
+
+    public struct ServiceAccountConfig
+    {
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+    }
+}
---
+++ decompile-security/Program.cs
@@ -0,0 +1,27 @@
+﻿// Initialize the Config object tat holds all of our example strings.
+Config config = new Config();
+
+Console.WriteLine("Hello, this is a trival application to demonstrate how to extract strings from the compiled binary \n");
+
+
+// Write out each property to the console
+Console.WriteLine("Database Configuration:");
+Console.WriteLine($"Username: {config.Database.Username}");
+Console.WriteLine($"Password: {config.Database.Password}");
+Console.WriteLine($"Host: {config.Database.Host}");
+Console.WriteLine($"Port: {config.Database.Port}");
+Console.WriteLine();
+
+Console.WriteLine("API Configuration:");
+Console.WriteLine($"Key: {config.Api.Key}");
+Console.WriteLine($"Endpoint: {config.Api.Endpoint}");
+Console.WriteLine();
+
+Console.WriteLine("Service Account Configuration:");
+Console.WriteLine($"ClientId: {config.ServiceAccount.ClientId}");
+Console.WriteLine($"ClientSecret: {config.ServiceAccount.ClientSecret}");
+Console.WriteLine();
+
+// Pause and wait for the user to press any key before exiting
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
